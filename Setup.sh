e1886b53157240bb4b687aee24f2232ed8e92d61
Stephen Padgham <stephenpadgham@yahoo.co.uk>
2024-04-26 19:26:24 +0100

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -14,9 +14,19 @@ done
 if [ "$control_node" = "no" ]; then
     read -p "Please provide the join string printed from your control-node: " join_str
 fi
-apt install sudo
+apt-get update && apt-get upgrade -y
+apt install -y sudo
 sudo apt-get update
+
 sudo apt-get install -y apt-transport-https apt-transport ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli
 
 wget https://go.dev/dl/go1.21.2.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.21.2.linux-amd64.tar.gz
